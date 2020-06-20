75c622085ad01181165799764f6d6b4e64bed39c
qadir <qadirabbasi1020@gmail.com>
2020-06-20 11:19:01 +0500

    Tutor_App changes 19/6/2020

--- app/src/main/java/com/example/tutor_app/Dashboard/ui/Searchfragment/FragmentSearch.java
+++ app/src/main/java/com/example/tutor_app/Dashboard/ui/Searchfragment/FragmentSearch.java
@@ -8,16 +8,15 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
-
-import com.example.tutor_app.Dashboard.ui.JobExperience.JobExperienceFragment;
+import android.widget.TextView;
 import com.example.tutor_app.Dashboard.ui.View.ViewFragment;
 import com.example.tutor_app.R;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 
@@ -28,6 +27,9 @@ public class FragmentSearch extends Fragment {
     private ArrayAdapter<String> arrayAdapterFeltter;
     private RelativeLayout rl_next;
     private FragmentTransaction fragmentTransaction;
+    private Spinner spinner_location,spinner_subject;
+    private static final String[] locations = {"Select your Location","Location1", "Location2", "Location3"};
+    private static final String[] subjects = {"Select your Subject","Subject1", "Subject2", "Subject3"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,15 +38,9 @@ public class FragmentSearch extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_search, container, false);
 
-//        consolidate_felter = new ArrayList<>();
-//        consolidate_felter.add("Select Criteria");
-//        consolidate_felter.add("Payment ID");
-//        consolidate_felter.add("Company");
-//        consolidate_felter.add("Status");
-//        consolidate_felter.add("Paid Date");
-//        consolidate_felter.add("Amount");
-//
-//        spinner_container_fees =  root.findViewById(R.id.spinner_container_fees);
+
+        spinner_location = (Spinner) root.findViewById(R.id.spinner_location);
+        spinner_subject = (Spinner) root.findViewById(R.id.spinner_subject);
 
         rl_next = root.findViewById(R.id.rl_next);
 
@@ -54,6 +50,83 @@ public class FragmentSearch extends Fragment {
                 fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
                 fragmentTransaction.replace(R.id.nav_host_fragment, new ViewFragment());
                 fragmentTransaction.commit();
+            }
+        });
+
+
+        ArrayAdapter<String> adapter_location = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item,locations) {
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                // TODO Auto-generated method stub
+                View view = super.getView(position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text1);
+                text.setTextColor(getResources().getColor(R.color.text_color_selection));
+                text.setTextSize((float) 13.6);
+                text.setPadding(30, 0, 30, 0);
+
+                return view;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                // TODO Auto-generated method stub
+                View view = super.getView(position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text1);
+                text.setTextColor(getResources().getColor(R.color.text_color_selection));
+                text.setTextSize((float) 13.6);
+                text.setPadding(30, 0, 30, 0);
+                return view;
+            }
+        };
+
+        spinner_location.setAdapter(adapter_location);
+        spinner_location.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<String> adapter_subject = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item,subjects) {
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                // TODO Auto-generated method stub
+                View view = super.getView(position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text1);
+                text.setTextColor(getResources().getColor(R.color.text_color_selection));
+                text.setTextSize((float) 13.6);
+                text.setPadding(30, 0, 30, 0);
+
+                return view;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                // TODO Auto-generated method stub
+                View view = super.getView(position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text1);
+                text.setTextColor(getResources().getColor(R.color.text_color_selection));
+                text.setTextSize((float) 13.6);
+                text.setPadding(30, 0, 30, 0);
+                return view;
+            }
+        };
+
+        spinner_subject.setAdapter(adapter_subject);
+        spinner_subject.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
 
