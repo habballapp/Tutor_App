0bb35f65666a81c9d15856338a4398182a0c4324
qadir <qadirabbasi1020@gmail.com>
2020-07-07 19:35:06 +0500

    Tutor_App changes 7/7/2020

--- app/src/main/java/com/example/tutor_app/Dashboard/ui/Searchfragment/FragmentSearch.java
+++ app/src/main/java/com/example/tutor_app/Dashboard/ui/Searchfragment/FragmentSearch.java
@@ -1,10 +1,15 @@
 package com.example.tutor_app.Dashboard.ui.Searchfragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +19,30 @@ import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.tutor_app.Adapters.MyAdapter_Child;
 import com.example.tutor_app.Dashboard.ui.Profile.Student.StateVO;
 import com.example.tutor_app.Dashboard.ui.View.ViewFragment;
+import com.example.tutor_app.MyJsonArrayRequest;
 import com.example.tutor_app.R;
+import com.example.tutor_app.Signin.SignIn;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class FragmentSearch extends Fragment {
@@ -30,7 +51,9 @@ public class FragmentSearch extends Fragment {
     private List<String> area,childs;
     private FragmentTransaction fragmentTransaction;
     private Spinner spinner_location,add_child;
+    private Map<String, String> childsMap = new HashMap<>();
     String Url = "http://pci.edusol.co/StudentPortal/searchtutorApi.php";
+    String userid;
 
 
     @Override
@@ -62,11 +85,7 @@ public class FragmentSearch extends Fragment {
         area.add("Lyari Town");
         area.add("Malir Town");
 
-        childs = new ArrayList<>();
-        childs.add("Select Child");
-        childs.add("child1");
-        childs.add("child1");
-        childs.add("child1");
+
 
 
 
@@ -123,21 +142,12 @@ public class FragmentSearch extends Fragment {
             }
         });
 
-        ArrayList<StateVO> listVOs = new ArrayList<>();
 
-        for (int i = 0; i < childs.size(); i++) {
-            StateVO stateVO = new StateVO();
-            stateVO.setTitle(childs.get(i));
-            stateVO.setSelected(false);
-            listVOs.add(stateVO);
+        try {
+            ViewChild();
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-
-        MyAdapter_Child myAdapter = new MyAdapter_Child(getContext(), android.R.layout.simple_spinner_dropdown_item, listVOs);
-        add_child.setAdapter(myAdapter);
-
-
-
 
 //        ArrayAdapter<String> adapter_subject = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item,childs) {
 //            @Override
@@ -176,12 +186,73 @@ public class FragmentSearch extends Fragment {
 //
 //            }
 //        });
-
-
         return root;
 
+    }
+
+    private void ViewChild() throws JSONException {
 
 
+        JSONObject map = new JSONObject();
+
+        SharedPreferences sharedPreferences1 = getContext().getSharedPreferences("LoginData",
+                Context.MODE_PRIVATE);
+        userid = sharedPreferences1.getString("userid", "");
+        Log.i("Id",userid);
+        map.put("userid",userid);
+
+        MyJsonArrayRequest sr = new MyJsonArrayRequest(Request.Method.POST, Url, map, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                childs = new ArrayList<>();
+                childs.add("Select Child");
+                Log.i("Search", String.valueOf(response));
+                for(int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject obj = new JSONObject(response.getString(i));
+                        childs.add(obj.getString("StudentName"));
+                        childsMap.put(obj.getString("StudentName"), obj.getString("Id"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                ArrayList<StateVO> listVOs = new ArrayList<>();
+
+                for (int i = 0; i < childs.size(); i++) {
+                    StateVO stateVO = new StateVO();
+                    stateVO.setTitle(childs.get(i));
+                    stateVO.setSelected(false);
+                    listVOs.add(stateVO);
+                }
+
+
+                MyAdapter_Child myAdapter = new MyAdapter_Child(getContext(), android.R.layout.simple_spinner_dropdown_item, listVOs);
+                add_child.setAdapter(myAdapter);
+
+
+
+
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> map = new HashMap<>();
+                map.put("Content-Type", "json");
+                return map;
+            }
+        };
+        Volley.newRequestQueue(getContext()).add(sr);
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(sr);
 
     }
 }
